9af94d6e1c671e74242aa4e92e0ae1b1aa60b5bb
r6915ee <astergthefourth@gmail.com>
2025-09-08 10:00:52 -0700

    feat: reenable xserver

--- system/services.nix
+++ system/services.nix
@@ -13,7 +13,7 @@
   
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = false;
+  services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
