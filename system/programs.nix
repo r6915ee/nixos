9e8e421ae5c4ff551f86a2f9892dc6e9aefe1b25
r6915ee <astergthefourth@gmail.com>
2025-09-07 22:58:27 -0700

    feat: add kde connect

--- system/programs.nix
+++ system/programs.nix
@@ -11,6 +11,8 @@
   # Install fish.
   programs.fish.enable = true;
 
+  programs.kdeconnect.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
