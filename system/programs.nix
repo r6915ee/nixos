6a7fd7a7284c8ad654c16c71082969b28a0cd74f
r6915ee <astergthefourth@gmail.com>
2025-09-13 14:48:25 -0700

    feat: add nh

--- system/programs.nix
+++ system/programs.nix
@@ -14,6 +14,12 @@
   # Enable KDE Connect.
   programs.kdeconnect.enable = true;
 
+  # Enable nh.
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
