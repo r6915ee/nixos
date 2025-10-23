473a414deeb9d57c36c4873a3ecd003eb8efb381
r6915ee <astergthefourth@gmail.com>
2025-10-22 23:02:43 -0700

    fix: remove xwaylandvideobridge exclusion
    Nixpkgs has recently removed xwaylandvideobridge from the repository.

--- system/environment.nix
+++ system/environment.nix
@@ -3,9 +3,5 @@
   environment.localBinInPath = true;
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    xwaylandvideobridge
-  ];
-
   systemd.user.sockets.gcr-ssh-agent.enable = true;
 }
