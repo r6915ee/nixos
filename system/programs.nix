746dcf8c509becae2c5c1c17d862b98e502dc8c4
r6915ee <astergthefourth@gmail.com>
2025-11-27 13:10:42 -0800

    refactor: move all program configuration to a single dictionary
    This also replaces Miracle with Niri.

--- system/programs.nix
+++ system/programs.nix
@@ -1,55 +1,57 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
-  # Install Steam.
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+  programs = {
+    # Install Steam.
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
 
-  programs.ssh.startAgent = true;
+    ssh.startAgent = true;
 
-  # Install fish.
-  programs.fish.enable = true;
+    # Install fish.
+    fish.enable = true;
 
-  # Install Git.
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
-    config = {
-      credential = {
-        helper = "libsecret";
+    # Install Git.
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+      config = {
+        credential = {
+          helper = "libsecret";
+        };
       };
     };
+
+    # Enable AppImages.
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
+
+    # Enable KDE Connect.
+    kdeconnect.enable = true;
+
+    # Enable Niri.
+    niri.enable = true;
+
+    # Enable HTOP.
+    htop.enable = true;
+
+    # Enable nh.
+    nh = {
+      enable = true;
+      clean.enable = true;
+    };
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
   };
-
-  # Enable AppImages.
-  programs.appimage = {
-    enable = true;
-    binfmt = true;
-  };
-
-  # Enable KDE Connect.
-  programs.kdeconnect.enable = true;
-
-  # Enable Miracle.
-  programs.wayland.miracle-wm.enable = true;
-
-  # Enable HTOP.
-  programs.htop.enable = true;
-
-  # Enable nh.
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 }
