bc0dbae4c200012c791a433fc476dd3318f774e0
r6915ee <astergthefourth@gmail.com>
2025-11-27 21:52:22 -0800

    feat: set quickshell icon theme to papirus

--- system/environment.nix
+++ system/environment.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 {
   environment.localBinInPath = true;
-  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+  environment.sessionVariables = {
+    COSMIC_DATA_CONTROL_ENABLED = 1;
+    QS_ICON_THEME = "Papirus-Dark";
+  };
 
   systemd.user.sockets.gcr-ssh-agent.enable = true;
 }
