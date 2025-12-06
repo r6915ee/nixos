9758e74da2a2c020579ab714df3eb57ae86cebdf
r6915ee <astergthefourth@gmail.com>
2025-12-05 20:49:41 -0800

    feat: setup home-manager

--- configuration.nix
+++ configuration.nix
@@ -4,53 +4,52 @@
 
 { pkgs, ... }:
 {
-  imports =
-    [
-      # Configure UUtils.
-      ./system/uutils.nix
-      # Use generic services.
-      # NOTE: Hardware-related services, specifically NVIDIA,
-      # are handled through their respective Nix file
-      # instead.
-      ./system/services.nix
-      # Include the results of the hardware scan.
-      ./hardware/scan.nix
-      # Set up the hardware attribute.
-      ./hardware/configuration.nix
-      # Manage users.
-      ./system/users.nix
-      # Manage security.
-      ./system/security.nix
-      # Manage fonts.
-      ./system/fonts.nix
-      # Manage system packages.
-      ./system/packages.nix
-      # Manage common programs.
-      ./system/programs.nix
-      # Handle networking.
-      ./system/networking.nix
-      # Set up Distrobox.
-      ./misc/distrobox.nix
-      # Set up virt-manager.
-      ./misc/virt-manager.nix
-      # Set up env. variables.
-      ./system/environment.nix
-      # Configure Plymouth.
-      ./misc/plymouth.nix
-      # Configure time and locale.
-      ./system/time-locale.nix
-    ];
+  imports = [
+    # Configure UUtils.
+    ./system/uutils.nix
+    # Use generic services.
+    # NOTE: Hardware-related services, specifically NVIDIA,
+    # are handled through their respective Nix file
+    # instead.
+    ./system/services.nix
+    # Include the results of the hardware scan.
+    ./hardware/scan.nix
+    # Set up the hardware attribute.
+    ./hardware/configuration.nix
+    # Manage users.
+    ./system/users.nix
+    # Manage security.
+    ./system/security.nix
+    # Manage fonts.
+    ./system/fonts.nix
+    # Manage system packages.
+    ./system/packages.nix
+    # Manage common programs.
+    ./system/programs.nix
+    # Handle networking.
+    ./system/networking.nix
+    # Set up Distrobox.
+    ./misc/distrobox.nix
+    # Set up virt-manager.
+    ./misc/virt-manager.nix
+    # Set up env. variables.
+    ./system/environment.nix
+    # Configure Plymouth.
+    ./misc/plymouth.nix
+    # Configure time and locale.
+    ./system/time-locale.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Experimental features
-  nix.settings.experimental-features = ["nix-command"];
-  
+  nix.settings.experimental-features = [ "nix-command" ];
+
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- system/users.nix
+++ system/users.nix
@@ -1,11 +1,22 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
+  imports = [ <home-manager/nixos> ];
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kolya = {
     isNormalUser = true;
     description = "Kolya";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     shell = pkgs.fish;
-    packages = [];
+    packages = [ ];
+  };
+  home-manager = {
+    users.kolya =
+      { ... }:
+      {
+        home.stateVersion = "25.05";
+      };
   };
 }
