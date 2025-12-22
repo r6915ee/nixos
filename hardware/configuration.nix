2644e0300781c5911bfb98d71d799c212abb47f1
r6915ee <astergthefourth@gmail.com>
2025-12-22 13:36:13 -0800

    fix: pin nvidia driver to v580.119.02

--- hardware/configuration.nix
+++ hardware/configuration.nix
@@ -21,7 +21,14 @@
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = false;
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "580.119.02";
+        sha256_64bit = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
+        sha256_aarch64 = "sha256-eYcYVD5XaNbp4kPue8fa/zUgrt2vHdjn6DQMYDl0uQs=";
+        openSha256 = "sha256-l3IQDoopOt0n0+Ig+Ee3AOcFCGJXhbH1Q1nh1TEAHTE=";
+        settingsSha256 = "sha256-sI/ly6gNaUw0QZFWWkMbrkSstzf0hvcdSaogTUoTecI=";
+        persistencedSha256 = "sha256-j74m3tAYON/q8WLU9Xioo3CkOSXfo1CwGmDx/ot0uUo=";
+      };
     };
 
     bluetooth = {
