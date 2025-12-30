12e6f8ce0450fe9c13dee02c4fa7370560dbf71d
r6915ee <astergthefourth@gmail.com>
2025-12-29 18:55:55 -0800

    feat: upgrade noctalia-shell to v3.8.0

--- system/custom/noctalia-shell.nix
+++ system/custom/noctalia-shell.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation rec {
   pname = "noctalia-shell";
-  version = "3.7.5";
+  version = "3.8.0";
   src = pkgs.fetchgit {
     url = "https://github.com/noctalia-dev/noctalia-shell.git";
     tag = "v${version}";
-    hash = "sha256-TBrsYS+n+8AsXRAP4wA3JNOdT604QmPo8yeSTT/p61I=";
+    hash = "sha256-Bh4XcEyG6XRQugahL/2Vd42k/YeGK0f+yW3+Oc74Rp4=";
   };
   dontConfigure = true;
   dontBuild = true;
