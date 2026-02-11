473400ec372b131d82fe4f816a14f491720339a7
r6915ee <astergthefourth@gmail.com>
2026-02-10 16:26:19 -0800

    feat: upgrade noctalia-shell to v4.4.1

--- system/custom/noctalia-shell.nix
+++ system/custom/noctalia-shell.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation rec {
   pname = "noctalia-shell";
-  version = "4.3.0";
+  version = "4.4.1";
   src = pkgs.fetchgit {
     url = "https://github.com/noctalia-dev/noctalia-shell.git";
     tag = "v${version}";
-    hash = "sha256-u+M2dCw9PznZTgn51DwHpX4VcU9ZC9Acub7qKhCpr3c=";
+    hash = "sha256-ozr4zO+T7o7FkPRTLpAymnb3K9+hc9OIN1lq0i5oWbs=";
   };
   dontConfigure = true;
   dontBuild = true;
